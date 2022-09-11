3186e018821be05328b0a76239b803f56d060357
Paul Gardner-Stephen <paul@servalproject.org>
2022-09-11 20:39:01 +0930

    improve i2c decoding

--- verilog/blink_megaphone/i2cdecode.c
+++ verilog/blink_megaphone/i2cdecode.c
@@ -4,10 +4,38 @@
 #include <string.h>
 #include <strings.h>
 
+int debug=0;
+
 unsigned char scl_char=0;
 unsigned char sda_char=0;
 
 int i2c_state=0;
+
+#define MAX_BYTES (1024*1024)
+  int i2c_bytes[MAX_BYTES];
+  int i2c_byte_count=0;
+
+int i2c_last_reg=0;
+
+void describe_transaction(void)
+{
+  if (!i2c_byte_count) return;
+
+  if (i2c_bytes[0]&1) {
+    // Read 
+    for(int i=1;i<i2c_byte_count;i++) {
+      fprintf(stderr,"INFO: Device $%02x,  read reg $%02x == $%02x\n",
+	      i2c_bytes[0]>>1,i2c_last_reg+i-1,i2c_bytes[i]);
+    }
+  } else {
+    for(int i=2;i<i2c_byte_count;i++) {
+      fprintf(stderr,"INFO: Device $%02x, WRITE reg $%02x <= $%02x\n",
+	      i2c_bytes[0]>>1,i2c_bytes[1]+i-2,i2c_bytes[i]);
+    }
+    // Work out which register will get read next
+    i2c_last_reg=i2c_bytes[1]+i2c_byte_count-2;
+  }
+}
 
 int main(int argc,char **argv)
 {
@@ -30,7 +58,7 @@ int main(int argc,char **argv)
 
   int i2c_bit=0;
   int i2c_byte=0;
-  
+
   char line[1024];
   line[0]=0; fgets(line,1024,f);
   while(line[0]) {
@@ -61,29 +89,33 @@ int main(int argc,char **argv)
       switch(i2c_state) {
       case IDLE:
 	if (scl_val=='1'&&sda_val=='0'&&sda_last=='1') {
-	  fprintf(stderr,"DEBUG: %16lld I2C START\n",timestamp);
+	  if (debug) fprintf(stderr,"DEBUG: %16lld I2C START\n",timestamp);
 	  i2c_state=START;
 	}
 	break;
       case START:
 	if (scl_val=='0'&&sda_val=='0'&&scl_last=='1') {
 	  // Clock gone low during start: Now we begin
-	  fprintf(stderr,"DEBUG: %16lld I2C START CLOCK LOW\n",timestamp);
+	  if (debug) fprintf(stderr,"DEBUG: %16lld I2C START CLOCK LOW\n",timestamp);
 	  i2c_state=XFER;
 	  i2c_bit=0;
 	  i2c_byte=0;
+	  i2c_byte_count=0;
 	}
 	break;
       case XFER:
 	if (scl_val=='1'&&scl_last=='1'&&sda_val=='0'&&sda_last=='1') {
 	  // RE-START detected
-	  fprintf(stderr,"DEBUG: %16lld I2C RE-START\n",timestamp);
+	  if (debug) fprintf(stderr,"DEBUG: %16lld I2C RE-START\n",timestamp);
+	  describe_transaction();
 	  i2c_state=START;
 	}
 	if (scl_val=='1'&&scl_last=='1'&&sda_val=='1'&&sda_last=='0') {
 	  // STOP detected
-	  fprintf(stderr,"DEBUG: %16lld I2C STOP\n",timestamp);
+	  if (debug) fprintf(stderr,"DEBUG: %16lld I2C STOP\n",timestamp);
+	  describe_transaction();
 	  i2c_state=IDLE;
+	  i2c_last_reg=0;
 	}
 	if (scl_val=='1'&&scl_last=='0') {
 	  // Clock gone high, latch bit
@@ -92,8 +124,11 @@ int main(int argc,char **argv)
 	  i2c_bit++;
 	  if (i2c_bit==9) {
 	    // End of byte
-	    fprintf(stderr,"DEBUG: %16lld I2C byte = $%02x (nack=%d)\n",
-		    timestamp,i2c_byte>>1,i2c_byte&1);
+	    if (debug) fprintf(stderr,"DEBUG: %16lld I2C byte = $%02x (nack=%d)\n",
+			       timestamp,i2c_byte>>1,i2c_byte&1);
+	    if (i2c_byte_count<MAX_BYTES) {
+	      i2c_bytes[i2c_byte_count++]=i2c_byte>>1;
+	    }
 	    i2c_state=XFER;
 	    i2c_bit=0;
 	    i2c_byte=0;
